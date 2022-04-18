b9b2e46c3bdbdb0b0beee6311b69316fd04d16be
Muhammad Rizki Syafapri <rizkisyafapri@gmail.com>
2022-04-18 14:44:53 +0700

    first commit

---
+++ app/src/main/java/com/mrsyafapri/roomwordsample/WordListAdapter.java
@@ -0,0 +1,39 @@
+package com.mrsyafapri.roomwordsample;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+public class WordListAdapter extends ListAdapter<Word, WordViewHolder> {
+
+    public WordListAdapter(@NonNull DiffUtil.ItemCallback<Word> diffCallback) {
+        super(diffCallback);
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return WordViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        Word current = getItem(position);
+        holder.bind(current.getWord());
+    }
+
+    static class WordDiff extends DiffUtil.ItemCallback<Word> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Word oldItem, @NonNull Word newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Word oldItem, @NonNull Word newItem) {
+            return oldItem.getWord().equals(newItem.getWord());
+        }
+    }
+}
